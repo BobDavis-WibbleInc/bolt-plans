96ce48afae4e000258ea863c15811b7932c0384e
Bob Davis <bob@wibble.net.nz>
2023-01-13 15:48:24 +1300

    test this

--- plans/myplan.pp
+++
@@ -1,9 +0,0 @@
-plan bdproj::myplan(
-    TargetSpec $targets,
-) {
-  
-  $puppet_master = "puppet.lan"
-  out::message("Backing up ${targets}")
-
-  run_task('exec', $puppet_master, 'command' => "hostname" )
-}
---
+++ plans/plan1.pp
@@ -0,0 +1,20 @@
+# @summary
+# A plan that runs a script
+
+plan ossg::plan1(
+    TargetSpec $targets,
+
+) {
+   $puppet_master = "puppet.lan"
+   $action = "status"
+   $name = "cockpit"
+
+   out::message("Backing up ${puppet_master}")
+   #run_task('exec', $puppet_master, 'command' => "hostname" )
+
+   #run_task('service', 'cockpit'=> "status", 'name' => "cockpit" )
+   run_task('service', $puppet_master, 'action' => "status", 'name' => "cockpit" )
+  }
+
+# puppet task run service action=status name=cockpit --nodes rhel8.lan
+# bolt task run service action=status name=cockpit --targets linux
